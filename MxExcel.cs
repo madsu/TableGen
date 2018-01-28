8981531cf6940813f095e2b0c0e58ff54bfbb080
wangzewei@hotmail.com <wangzewei>
2018-01-28 15:16:05 +0800

    解决编译错误

--- MxExcel.cs
+++ MxExcel.cs
@@ -38,8 +38,8 @@ namespace MxExcelTool
             {
                 for (int i = 1; i < fastExcel.MaxSheetNumber; i++)
                 {
-                    Worksheet worksheet = fastExcel.Read(sheetNames[i]);
-                    if ( null == worksheet ) continue;
+                  //  Worksheet worksheet = fastExcel.Read();
+                  //  if ( null == worksheet ) continue;
 //                     var row =worksheet.Rows.GetEnumerator();
 //                     while ( row.MoveNext() ) {
 //                         Row  rw= row.Current;
@@ -50,39 +50,11 @@ namespace MxExcelTool
 //                             Log(string.Format("列 {0}  值 {1}" , cel.ColumnNumber , cel.Value.ToString()));
 //                         }
 //                     }
-                    sheets.Add(worksheet);
+         //           sheets.Add(worksheet);
                 }
             }
 
             Log(string.Format("Reading data took {0} seconds", stopwatch.Elapsed.TotalSeconds));
-            return sheets;
-        }
-
-        static public List<Worksheet> FastExcelRead(FileInfo inputFile , bool readOnly = true)
-        {
-            List<Worksheet> sheets = new List<Worksheet>();
-            Stopwatch stopwatch = Stopwatch.StartNew();
-
-            // Open excel file using read only is much faster, but you cannot perfrom any writes
-            using ( FastExcel.FastExcel fastExcel = new FastExcel.FastExcel(inputFile , readOnly) ) {
-                for ( int i = 1 ; i < sheetCount ; i++ ) {
-                    Worksheet worksheet = fastExcel.Read(i);
-                    if ( null == worksheet ) continue;
-                    //                     var row =worksheet.Rows.GetEnumerator();
-                    //                     while ( row.MoveNext() ) {
-                    //                         Row  rw= row.Current;
-                    //                         Log(string.Format("行 {0} " , rw.RowNumber ));
-                    //                         var cell = rw.Cells.GetEnumerator();
-                    //                         while ( cell.MoveNext() ) {
-                    //                             Cell  cel =cell.Current;
-                    //                             Log(string.Format("列 {0}  值 {1}" , cel.ColumnNumber , cel.Value.ToString()));
-                    //                         }
-                    //                     }
-                    sheets.Add(worksheet);
-                }
-            }
-
-            Log(string.Format("Reading data took {0} seconds" , stopwatch.Elapsed.TotalSeconds));
             return sheets;
         }
 
